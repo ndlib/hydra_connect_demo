b352c7ea1f544d9e0e51df4b942318a566f73a4a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-08-22 12:55:58 -0400

    Adding hydramata-works migrations
    $ rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,58 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140822152161) do
+
+  create_table "hydramata_works_predicate_presentation_sequences", force: true do |t|
+    t.integer  "predicate_set_id",      null: false
+    t.integer  "predicate_id",          null: false
+    t.integer  "presentation_sequence", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "hydramata_works_predicate_presentation_sequences", ["predicate_set_id", "predicate_id"], name: "hydramata_works_predicate_presentation_sequences_identity", unique: true
+  add_index "hydramata_works_predicate_presentation_sequences", ["predicate_set_id", "presentation_sequence"], name: "hydramata_works_predicate_presentation_sequences_presentation", unique: true
+
+  create_table "hydramata_works_predicate_sets", force: true do |t|
+    t.integer  "work_type_id",               null: false
+    t.string   "identity",                   null: false
+    t.integer  "presentation_sequence",      null: false
+    t.string   "name_for_application_usage"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "hydramata_works_predicate_sets", ["work_type_id", "identity"], name: "hydramata_works_predicate_set_identity", unique: true
+  add_index "hydramata_works_predicate_sets", ["work_type_id", "presentation_sequence"], name: "hydramata_works_predicate_set_sequence", unique: true
+
+  create_table "hydramata_works_predicates", force: true do |t|
+    t.string   "identity",                   null: false
+    t.string   "name_for_application_usage"
+    t.string   "datastream_name"
+    t.string   "value_coercer_name"
+    t.string   "value_parser_name"
+    t.string   "indexing_strategy"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.text     "validations"
+    t.string   "itemprop_schema_dot_org"
+  end
+
+  create_table "hydramata_works_types", force: true do |t|
+    t.string   "identity",                   null: false
+    t.string   "name_for_application_usage"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "itemtype_schema_dot_org"
+  end
+
+  create_table "hydramata_works_works", id: false, force: true do |t|
+    t.string   "pid",                           null: false
+    t.string   "work_type",                     null: false
+    t.text     "properties", limit: 2147483647
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
