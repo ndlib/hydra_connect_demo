cdc22a1148ba00116547f6c77f634ad8883709f1
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-08-28 10:46:56 -0400

    Migrating database for active_admin
    $ rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140822152161) do
+ActiveRecord::Schema.define(version: 20140828144546) do
+
+  create_table "active_admin_comments", force: true do |t|
+    t.string   "namespace"
+    t.text     "body"
+    t.string   "resource_id",   null: false
+    t.string   "resource_type", null: false
+    t.integer  "author_id"
+    t.string   "author_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace"
+  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
 
   create_table "hydramata_works_predicate_presentation_sequences", force: true do |t|
     t.integer  "predicate_set_id",      null: false
