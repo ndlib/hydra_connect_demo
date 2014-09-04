330cfb62df01f8427cb331616db31f54e524480c
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-09-04 15:45:05 -0400

    Updating migrations for Hydramata::Works
    $ rake hydramata_works:install:migrations

---
+++ db/migrate/20140904194349_create_hydramata_works_attachments.hydramata_works.rb
@@ -0,0 +1,15 @@
+# This migration comes from hydramata_works (originally 20140904185554)
+class CreateHydramataWorksAttachments < ActiveRecord::Migration
+  def change
+    create_table :hydramata_works_attachments, id: false do |t|
+      t.string :pid, null: false
+      t.string :work_id
+      t.string :predicate
+      t.string :attachment_uid
+      t.timestamps
+    end
+    add_index :hydramata_works_attachments, :pid, unique: true
+    add_index :hydramata_works_attachments, :work_id
+    add_index :hydramata_works_attachments, :predicate
+  end
+end
