0ff33256cd551b345e66dbaa4469510a9fdd66c7
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-09-05 15:38:41 -0400

    Pushing out SitePrism specs

--- spec/features/works/lifecycle_feature_spec.rb
+++ spec/features/works/lifecycle_feature_spec.rb
@@ -6,16 +6,17 @@ module SitePrism::Within
   end
 end
 
-class AvailableTypesPage < SitePrism::Page
-  extend SitePrism::Within
+SitePrism::Page.extend(SitePrism::Within)
+
+
+class WorksAvailableTypesPage < SitePrism::Page
   set_url '/works/available_types'
   element :new_document_link, %(.work-type .btn[href="/works/document/new"])
   element :new_article_link, %(.work-type .btn[href="/works/article/new"])
   elements :new_link_container, '.available-types .work-type'
 end
 
-class NewWorkPage < SitePrism::Page
-  extend SitePrism::Within
+class WorkNewPage < SitePrism::Page
   set_url_matcher %r{/works/\w+/new}
   element :required_fieldset, 'form.work fieldset.required caption'
   element :dc_title_label, 'form.work fieldset.required #label_for_work_dc_title'
@@ -29,42 +30,55 @@ class NewWorkPage < SitePrism::Page
   element :submit_button, 'form.work .actions input[name="commit"]'
 end
 
+class WorkShowPage < SitePrism::Page
+  elements :dc_title, '.required .metadata .value.dc-title'
+  elements :dc_abstract, '.required .metadata .value.dc-abstract'
+  elements :attachment, '.optional .metadata .value.attachment'
+  elements :actions, '.actions'
+
+  def edit_link
+    find(%(.actions a[href="/works/#{identity}/edit"]))
+  end
+
+  def identity
+    current_path.split("/").last
+  end
+end
+
 feature 'Works#available_types page' do
   before do
     load "#{Rails.root}/db/seeds.rb"
   end
 
   scenario 'Creating a work with valid data' do
-    AvailableTypesPage.within do |page|
-      page.load
-      expect(page).to be_all_there
-      page.new_article_link.click
+    WorksAvailableTypesPage.within do |the_page|
+      the_page.load
+      expect(the_page).to be_all_there
+      the_page.new_article_link.click
     end
 
-    NewWorkPage.within do |page|
-      expect(page).to be_all_there
-      page.dc_title_input.set('My Title')
-      page.dc_abstract_input.set('My Abstract')
-      page.attachment_input.set(
+    WorkNewPage.within do |the_page|
+      expect(the_page).to be_all_there
+      the_page.dc_title_input.set('My Title')
+      the_page.dc_abstract_input.set('My Abstract')
+      the_page.attachment_input.set(
         [
           File.expand_path('../../../../LICENSE', __FILE__),
           File.expand_path('../../../../README.md', __FILE__)
         ]
       )
-      page.submit_button.click
+      the_page.submit_button.click
     end
 
     # @TODO - Message saying work was created
 
-    # Show Work
-    identity = page.current_path.split("/").last
-    expect(page).to have_css('.required .metadata .value.dc-title', text: 'My Title')
-    expect(page).to have_css('.required .metadata .value.dc-abstract', text: 'My Abstract')
-    expect(page).to have_css('.optional .metadata .value.attachment a', text: 'README.md')
-    expect(page).to have_css('.optional .metadata .value.attachment a', text: 'LICENSE')
-    expect(page).to have_css(%(.actions a[href="#{edit_work_path(identity)}"]), text: "Edit this Article")
+    show_page = WorkShowPage.within do |the_page|
+      expect(the_page.dc_title.map(&:text)).to eq(['My Title'])
+      expect(the_page.dc_abstract.map(&:text)).to eq(['My Abstract'])
+      expect(the_page.attachment.map(&:text)).to eq(['README.md', 'LICENSE'])
+      the_page.edit_link.click
+    end
 
-    find(%(.actions a[href="#{edit_work_path(identity)}"])).click
     # Edit Work
     within('form.work') do
       expect(page).to have_css('fieldset.required caption', text: 'Required Article Attributes')
@@ -82,17 +96,17 @@ feature 'Works#available_types page' do
     end
 
     updated_identity = page.current_path.split("/").last
-    expect(updated_identity).to eq(identity)
+    expect(updated_identity).to eq(show_page.identity)
     expect(page).to have_css('.required .metadata .value.dc-title', text: 'My Title')
     expect(page).to have_css('.required .metadata .value.dc-title', text: 'Another Title')
     expect(page).to have_css('.required .metadata .value.dc-abstract', text: 'My Abstract')
     expect(page).to have_css('.required .metadata .value.dc-abstract', text: 'Another Abstract')
-    expect(page).to have_css(%(.actions a[href="#{edit_work_path(identity)}"]), text: "Edit this Article")
+    expect(page).to have_css(%(.actions a[href="#{edit_work_path(updated_identity)}"]), text: "Edit this Article")
   end
 
   scenario 'Creating a work with invalid data' do
     # Select Work Type
-    available_types_page = AvailableTypesPage.new
+    available_types_page = WorksAvailableTypesPage.new
     available_types_page.load
 
     expect(available_types_page).to be_all_there
