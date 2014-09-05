c4aca9ed779c1dd070cac4c1a2ad7319ad83f811
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-09-05 15:13:09 -0400

    Working on extracting SitePrism::Page
    Instead of lots of convoluted things, creating page representations.
    This should help reduce the fragility of the page process.

--- spec/features/works/lifecycle_feature_spec.rb
+++ spec/features/works/lifecycle_feature_spec.rb
@@ -1,9 +1,32 @@
 require 'site_prism'
 
+module SitePrism::Within
+  def within(*args)
+    new(*args).tap {|page| yield(page) }
+  end
+end
+
 class AvailableTypesPage < SitePrism::Page
+  extend SitePrism::Within
   set_url '/works/available_types'
   element :new_document_link, %(.work-type .btn[href="/works/document/new"])
   element :new_article_link, %(.work-type .btn[href="/works/article/new"])
+  elements :new_link_container, '.available-types .work-type'
+end
+
+class NewWorkPage < SitePrism::Page
+  extend SitePrism::Within
+  set_url_matcher %r{/works/\w+/new}
+  element :required_fieldset, 'form.work fieldset.required caption'
+  element :dc_title_label, 'form.work fieldset.required #label_for_work_dc_title'
+  element :dc_title_input, 'form.work fieldset.required #work_dc_title_0'
+  element :dc_abstract_label, 'form.work fieldset.required #label_for_work_dc_abstract'
+  element :dc_abstract_input, 'form.work fieldset.required #work_dc_abstract_0'
+
+  element :optional_fieldset, 'form.work fieldset.optional caption'
+  element :attachment_input, 'form.work fieldset.optional #work_attachment_0'
+
+  element :submit_button, 'form.work .actions input[name="commit"]'
 end
 
 feature 'Works#available_types page' do
@@ -12,37 +35,23 @@ feature 'Works#available_types page' do
   end
 
   scenario 'Creating a work with valid data' do
-    available_types_page = AvailableTypesPage.new
-    available_types_page.load
-    expect(available_types_page).to have_css('.available-types .work-type .name', count: 2)
-    expect(available_types_page).to have_css('.available-types .work-type .description', count: 2)
-    expect(available_types_page).to have_new_document_link
-    expect(available_types_page).to have_new_article_link
-    available_types_page.new_article_link.click
+    AvailableTypesPage.within do |page|
+      page.load
+      expect(page).to be_all_there
+      page.new_article_link.click
+    end
 
-    # New Work
-    within('form.work') do
-      expect(page).to have_css('fieldset.required caption', text: 'Required Article Attributes')
-      within('fieldset.required') do
-        expect(page).to have_css('#label_for_work_dc_title', text: 'Title')
-        fill_in('work_dc_title_0', with: 'My Title')
-        expect(page).to have_css('#label_for_work_dc_abstract', text: 'Abstract')
-        fill_in('work_dc_abstract_0', with: 'My Abstract')
-      end
-
-      within('fieldset.optional') do
-        expect(page).to have_css('#label_for_work_attachment', text: I18n.t('hydramata.core.properties.attachment.label'))
-        attach_file(
-          'work_attachment_0',
-          [
-            File.expand_path('../../../../LICENSE', __FILE__),
-            File.expand_path('../../../../README.md', __FILE__)
-          ]
-        )
-      end
-
-      # Create Work
-      find('.actions input[name="commit"]').click
+    NewWorkPage.within do |page|
+      expect(page).to be_all_there
+      page.dc_title_input.set('My Title')
+      page.dc_abstract_input.set('My Abstract')
+      page.attachment_input.set(
+        [
+          File.expand_path('../../../../LICENSE', __FILE__),
+          File.expand_path('../../../../README.md', __FILE__)
+        ]
+      )
+      page.submit_button.click
     end
 
     # @TODO - Message saying work was created
@@ -85,10 +94,8 @@ feature 'Works#available_types page' do
     # Select Work Type
     available_types_page = AvailableTypesPage.new
     available_types_page.load
-    expect(available_types_page).to have_css('.available-types .work-type .name', count: 2)
-    expect(available_types_page).to have_css('.available-types .work-type .description', count: 2)
-    expect(available_types_page).to have_new_document_link
-    expect(available_types_page).to have_new_article_link
+
+    expect(available_types_page).to be_all_there
     available_types_page.new_article_link.click
 
     # New Work
