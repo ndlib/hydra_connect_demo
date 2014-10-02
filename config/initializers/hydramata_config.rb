ffa50478b4775d8861af22a6619000f7e7d32e72
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-10-02 08:33:06 -0400

    Tweaking initializer for live demo capability

--- config/initializers/hydramata_config.rb
+++ config/initializers/hydramata_config.rb
@@ -10,4 +10,18 @@ Hydramata.configure do |config|
       work
     end
   end
+
+  if ENV['JETTY_TO_DB']
+    require 'hydramata/works/from_persistence/fedora_wrangler'
+    config.work_from_persistence_coordinator = ->(options) do
+      work = Hydramata::Works::Work.new(identity: options.fetch(:pid))
+      Hydramata::Works::FromPersistence::FedoraWrangler.new(work: work).call(work.identity, with_datastreams: true)
+      work
+    end
+  end
+
+  if ENV['DB_TO_JETTY']
+    require 'hydramata/works/rubydora_persistence_coordinator'
+    config.work_to_persistence_coordinator = Hydramata::Works::RubydoraPersistenceCoordinator
+  end
 end
